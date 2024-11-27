e4382b94847c1d6dabf51ba9015491aa4ef79c8a
Thankful Python <167735173+pydev11@users.noreply.github.com>
2024-11-27 13:39:49 -0500

    Update main.py

--- macrofiles/main.py
+++ macrofiles/main.py
@@ -4,3 +4,27 @@ import requests
 import json
 import numpy
 import eel
+
+
+print("Def Printing")
+
+try:
+    import robloxpy
+    print("Module 'robloxpy' imported successfully.")
+    print("RobloxPY version = 0.2.21")
+except ImportError:
+    print("Module 'robloxpy' is not installed. Please install it using 'pip3 install robloxpy'.")
+
+
+
+wait(1)
+
+
+
+
+try:
+    import pyatogui
+    print("Module 'pyatogui' imported successfully.")
+    print("pyatogui version = 0.9.54")
+except ImportError:
+    print("Module 'pyatogui' is not installed. Please install it using 'pip3 install robloxpy'.")
