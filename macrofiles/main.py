1dd876a4ab8d58cbf1b62bdc14884574793f5bd2
Thankful Python <167735173+pydev11@users.noreply.github.com>
2024-11-27 13:50:23 -0500

    Update main.py

--- macrofiles/main.py
+++ macrofiles/main.py
@@ -15,15 +15,31 @@ except ImportError:
     print("Module 'robloxpy' is not installed. Please install it using 'pip3 install robloxpy'.")
 
 
-
-wait(1)
-
-
-
-
 try:
-    import pyatogui
+    import requests
     print("Module 'pyatogui' imported successfully.")
     print("pyatogui version = 0.9.54")
 except ImportError:
     print("Module 'pyatogui' is not installed. Please install it using 'pip3 install robloxpy'.")
+
+
+try:
+    import json
+    print("Module 'json' imported successfully.")
+except ImportError:
+    print("Module 'json' is not installed. Please install it using 'pip3 install json'.")\
+    
+
+try:
+    import numpy
+    print("Module 'numpy' imported successfully.")
+except ImportError:
+    print("Module 'numpy' is not installed. Please install it using 'pip3 install numpy'.")
+
+
+
+try:
+    import eel
+    print("Module 'eel' imported successfully.")
+except ImportError:
+    print("Module 'eel' is not installed. Please install it using 'pip3 install eel'.")
